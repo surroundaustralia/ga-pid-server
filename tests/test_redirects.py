2b84bcc9d12580daa01fff3d322f966a8daceb7b
adamdavis2 <97263177+adamdavis2@users.noreply.github.com>
2022-02-01 07:08:34 +0800

    Update test_redirects.py

--- tests/test_redirects.py
+++ tests/test_redirects.py
@@ -8,7 +8,7 @@ def redirect(label, from_, headers=None):
     return r.headers.get("location")
 
 
-def create_cases(json_file, host="http://localhost:80"):
+def create_cases(json_file, host="http://pid.geoscience.gov.au"):
     test_cases = []
     uris = json.load(open(json_file, "r"))
     for uri, cases in uris.items():
@@ -18,11 +18,11 @@ def create_cases(json_file, host="http://localhost:80"):
     return test_cases
 
 
-@pytest.mark.parametrize("test_input,expected,label,headers", create_cases("rule-cases.json"))
+@pytest.mark.parametrize("test_input,expected,label,headers", create_cases("/usr/local/PID_tests/rule-cases.json"))
 def test_pid(test_input, expected, label, headers):
     assert redirect(label=label, from_=test_input, headers=headers) == expected, label
 
 
-@pytest.mark.parametrize("test_input,expected,label,headers", create_cases("extra-rule-cases.json"))
-def test_extra_pid(test_input, expected, label, headers):
-    assert redirect(label=label, from_=test_input, headers=headers) == expected, label
+#@pytest.mark.parametrize("test_input,expected,label,headers", create_cases("extra-rule-cases.json"))
+#def test_extra_pid(test_input, expected, label, headers):
+#    assert redirect(label=label, from_=test_input, headers=headers) == expected, label
